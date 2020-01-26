23a4db6c335ac78a2489af9d7c6f6941123ed730
Techwinx1 <einnocent99@gmail.com>
2020-01-26 11:55:28 -0800

    save all files

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 //creat a secret number
-var secretNumber = 5;  
+const secretNumber = 5;
 
 //ask user for guess
-var guess = prompt("guess a number");
+const guess = prompt("guess a number");
 
 // check guess
-if(Number (guess)=== secretNumber) {
+if (Number(guess) === secretNumber) {
     alert("YOU GOT IT RIGHT!");
-}else if (Number (guess) > secretNumber){
+}
+else if (Number(guess) > secretNumber) {
     alert("Number too high..guess again");
-}else if(Number (guess) < secretNumber){
+}
+else if (Number(guess) < secretNumber) {
     alert("Number too low..guess again");
-}else{
+}
+else {
     alert("C'mon ..you can do this..try again");
 }
